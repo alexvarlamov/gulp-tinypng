8bf262a310d243845bf72278601d78eb7dca6cf5
Ihor Haiduk <cookkycok@gmail.com>
2017-10-13 00:04:27 +0300

    update for node v8+

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.gulpTiny = void 0;
 
-var _regenerator = require("babel-runtime/regenerator"),
-    _regenerator2 = _interopRequireDefault(_regenerator),
-    _asyncToGenerator2 = require("babel-runtime/helpers/asyncToGenerator"),
+var _asyncToGenerator2 = require("babel-runtime/helpers/asyncToGenerator"),
     _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2),
     _promise = require("babel-runtime/core-js/promise"),
     _promise2 = _interopRequireDefault(_promise),
@@ -29,27 +27,30 @@ var _regenerator = require("babel-runtime/regenerator"),
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var PLUGIN_NAME = "gulp-tiny",
-    TEMP_DIR = '.gulp/tinypng',
-    CURRENT_KEY = 0,
+const PLUGIN_NAME = "gulp-tiny",
+      TEMP_DIR = '.gulp/tinypng';
+
+let CURRENT_KEY = 0,
     CURRENT_COUNT = null,
-    MAX_COUNT = 499,
-    cleanTemp = function cleanTemp() {
-	(0, _rmdir2.default)(TEMP_DIR, function (err) {
+    MAX_COUNT = 499;
+
+
+const cleanTemp = () => {
+	(0, _rmdir2.default)(TEMP_DIR, err => {
 		if (err) {
 			console.error('Error creating temp folder');
 		}
-		(0, _mkdirp2.default)(TEMP_DIR, function (err) {
+		(0, _mkdirp2.default)(TEMP_DIR, err => {
 			if (err) {
 				console.error('Error creating temp folder');
 			}
 		});
 	});
 },
-    createTempDir = function createTempDir() {
-	(0, _fs.access)(TEMP_DIR, _fs.F_OK, function (err) {
+      createTempDir = () => {
+	(0, _fs.access)(TEMP_DIR, _fs.F_OK, err => {
 		if (err) {
-			(0, _mkdirp2.default)(TEMP_DIR, function (err) {
+			(0, _mkdirp2.default)(TEMP_DIR, err => {
 				if (err) {
 					console.error('Error creating temp folder');
 				}
@@ -57,280 +58,178 @@ var PLUGIN_NAME = "gulp-tiny",
 		}
 	});
 },
-    readTemp = function readTemp(filename, cb) {
-	(0, _fs.readFile)((0, _path2.join)(TEMP_DIR, filename), function (err, data) {
+      readTemp = (filename, cb) => {
+	(0, _fs.readFile)((0, _path2.join)(TEMP_DIR, filename), (err, data) => {
 		if (err) {
 			return cb(new _gulpUtil.PluginError(PLUGIN_NAME, err));
 		}
 		cb(null, data);
 	});
 },
-    download = function download(uri, filename) {
-	return new _promise2.default(function (resolve, reject) {
-		_request2.default.head(uri, function (err) {
+      download = (uri, filename) => {
+	return new _promise2.default((resolve, reject) => {
+		_request2.default.head(uri, err => {
 			if (err) {
 				reject(err);
 			} else {
-				var path = (0, _path2.join)(TEMP_DIR, filename);
+				const path = (0, _path2.join)(TEMP_DIR, filename);
 				(0, _request2.default)({ url: uri, strictSSL: !1 }).pipe((0, _fs.createWriteStream)(path)).on('close', resolve);
 			}
 		});
 	});
 },
-    compile = function () {
-	var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(file) {
-		var size = arguments.length > 1 && arguments[1] !== void 0 ? arguments[1] : [],
-		    apiKey = arguments[2],
-		    cb = arguments[3],
-		    data_return = void 0,
-		    AUTH_TOKEN = void 0;
-		return _regenerator2.default.wrap(function _callee2$(_context2) {
-			while (1) {
-				switch (_context2.prev = _context2.next) {
-					case 0:
-						_context2.prev = 0;
-						data_return = [], AUTH_TOKEN = new Buffer('api:' + apiKey).toString('base64');
+      compile = (() => {
+	var _ref = (0, _asyncToGenerator3.default)(function* (file, size = [], apiKey, cb) {
+		try {
+			const data_return = [],
+			      AUTH_TOKEN = new Buffer('api:' + apiKey).toString('base64');
 
-						(0, _request2.default)({
-							url: 'https://api.tinypng.com/shrink',
-							method: 'POST',
-							strictSSL: !1,
-							headers: {
-								Accept: '*/*',
-								"Cache-Control": 'no-cache',
-								"Content-Type": 'application/x-www-form-urlencoded',
-								Authorization: 'Basic ' + AUTH_TOKEN
-							},
-							body: file.contents
-						}, function () {
-							var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(err, resp, body) {
-								var answer, answ, count, filename, nsize;
-								return _regenerator2.default.wrap(function _callee$(_context) {
-									while (1) {
-										switch (_context.prev = _context.next) {
-											case 0:
-												_context.prev = 0;
+			(0, _request2.default)({
+				url: 'https://api.tinypng.com/shrink',
+				method: 'POST',
+				strictSSL: !1,
+				headers: {
+					Accept: '*/*',
+					"Cache-Control": 'no-cache',
+					"Content-Type": 'application/x-www-form-urlencoded',
+					Authorization: 'Basic ' + AUTH_TOKEN
+				},
+				body: file.contents
+			}, (() => {
+				var _ref2 = (0, _asyncToGenerator3.default)(function* (err, resp, body) {
+					try {
+						if (err !== null) throw new _gulpUtil.PluginError(PLUGIN_NAME, err);
+						const answer = JSON.parse(body),
+						      answ = resp.toJSON(),
+						      count = parseInt(answ.headers['compression-count']);
 
-												if (!(err !== null)) {
-													_context.next = 3;
-													break;
-												}
+						CURRENT_COUNT = count;
 
-												throw new _gulpUtil.PluginError(PLUGIN_NAME, err);
-
-											case 3:
-												answer = JSON.parse(body), answ = resp.toJSON(), count = parseInt(answ.headers['compression-count']);
-
-												CURRENT_COUNT = count;
-
-												if (!(answer.output && answer.output.url)) {
-													_context.next = 13;
-													break;
-												}
-
-												filename = (0, _md2.default)(file.contents);
-												_context.next = 9;
-												return download(answer.output.url, filename);
-
-											case 9:
-												_context.next = 11;
-												return new _promise2.default(function (resolve) {
-													(0, _fs.readFile)((0, _path2.join)(TEMP_DIR, filename), function (err, content) {
-														if (err) throw new _gulpUtil.PluginError(PLUGIN_NAME, err);
-														data_return.push({ content: content });
-														resolve();
-													});
-												});
-
-											case 11:
-												nsize = size.map(function (_ref3) {
-													var name = _ref3.name,
-													    method = _ref3.method,
-													    width = _ref3.width,
-													    height = _ref3.height;
-
-													return new _promise2.default(function (resolve) {
-														(0, _request2.default)({
-															url: answer.output.url,
-															method: 'GET',
-															strictSSL: !1,
-															headers: {
-																Accept: '*/*',
-																"Cache-Control": 'no-cache',
-																"Content-Type": 'application/json',
-																Authorization: 'Basic ' + AUTH_TOKEN
-															},
-															json: {
-																resize: { method: method, width: width, height: height }
-															}
-														}, function (err, resp) {
-															if (!err) {
-																var _answ = resp.toJSON(),
-																    _count = parseInt(_answ.headers['compression-count']);
-
-																CURRENT_COUNT = _count;
-															}
-														}).pipe((0, _fs.createWriteStream)((0, _path2.join)(TEMP_DIR, filename + "_" + name))).on('close', function () {
-															data_return.push({ filename: filename + "_" + name, folder: name });
-															resolve();
-														});
-													});
-												});
-
-												_promise2.default.all(nsize).then(function () {
-													cb(data_return);
-												});
-
-											case 13:
-												_context.next = 18;
-												break;
-
-											case 15:
-												_context.prev = 15;
-												_context.t0 = _context["catch"](0);
-												throw new _gulpUtil.PluginError(PLUGIN_NAME, _context.t0);
-
-											case 18:
-											case "end":
-												return _context.stop();
+						if (answer.output && answer.output.url) {
+							const filename = (0, _md2.default)(file.contents);
+							yield download(answer.output.url, filename);
+							yield new _promise2.default(function (resolve) {
+								(0, _fs.readFile)((0, _path2.join)(TEMP_DIR, filename), function (err, content) {
+									if (err) throw new _gulpUtil.PluginError(PLUGIN_NAME, err);
+									data_return.push({ content });
+									resolve();
+								});
+							});
+							const nsize = size.map(function ({ name, method, width, height }) {
+								return new _promise2.default(function (resolve) {
+									(0, _request2.default)({
+										url: answer.output.url,
+										method: 'GET',
+										strictSSL: !1,
+										headers: {
+											Accept: '*/*',
+											"Cache-Control": 'no-cache',
+											"Content-Type": 'application/json',
+											Authorization: 'Basic ' + AUTH_TOKEN
+										},
+										json: {
+											resize: { method, width, height }
 										}
-									}
-								}, _callee, void 0, [[0, 15]]);
-							}));
+									}, function (err, resp) {
+										if (!err) {
+											const answ = resp.toJSON(),
+											      count = parseInt(answ.headers['compression-count']);
 
-							return function (_x3, _x4, _x5) {
-								return _ref2.apply(this, arguments);
-							};
-						}());
-						_context2.next = 8;
-						break;
+											CURRENT_COUNT = count;
+										}
+									}).pipe((0, _fs.createWriteStream)((0, _path2.join)(TEMP_DIR, filename + "_" + name))).on('close', function () {
+										data_return.push({ filename: filename + "_" + name, folder: name });
+										resolve();
+									});
+								});
+							});
+							_promise2.default.all(nsize).then(function () {
+								cb(data_return);
+							});
+						}
+					} catch (err) {
+						throw new _gulpUtil.PluginError(PLUGIN_NAME, err);
+					}
+				});
 
-					case 5:
-						_context2.prev = 5;
-						_context2.t0 = _context2["catch"](0);
-						throw new _gulpUtil.PluginError(PLUGIN_NAME, _context2.t0);
-
-					case 8:
-					case "end":
-						return _context2.stop();
-				}
-			}
-		}, _callee2, void 0, [[0, 5]]);
-	}));
+				return function (_x2, _x3, _x4) {
+					return _ref2.apply(this, arguments);
+				};
+			})());
+		} catch (error) {
+			throw new _gulpUtil.PluginError(PLUGIN_NAME, error);
+		}
+	});
 
 	return function compile(_x) {
 		return _ref.apply(this, arguments);
 	};
-}(),
-    compileTest = function () {
-	var _ref4 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee3(keys, file, size, cb) {
-		var apiKey;
-		return _regenerator2.default.wrap(function _callee3$(_context3) {
-			while (1) {
-				switch (_context3.prev = _context3.next) {
-					case 0:
-						_context3.prev = 0;
-						_context3.next = 3;
-						return new _promise2.default(function (resolve) {
-							if (CURRENT_COUNT == null) {
-								var apiKey = keys[CURRENT_KEY],
-								    AUTH_TOKEN = new Buffer('api:' + apiKey).toString('base64');
+})(),
+      compileTest = (() => {
+	var _ref3 = (0, _asyncToGenerator3.default)(function* (keys, file, size, cb) {
+		try {
+			yield new _promise2.default(function (resolve) {
+				if (CURRENT_COUNT == null) {
+					const apiKey = keys[CURRENT_KEY],
+					      AUTH_TOKEN = new Buffer('api:' + apiKey).toString('base64');
 
 
-								(0, _request2.default)({
-									url: 'https://api.tinypng.com/shrink',
-									method: 'POST',
-									strictSSL: !1,
-									headers: {
-										Accept: '*/*',
-										"Cache-Control": 'no-cache',
-										Authorization: 'Basic ' + AUTH_TOKEN
-									}
-								}, function (error, response, body) {
-									try {
-										if (error !== null) throw new _gulpUtil.PluginError(PLUGIN_NAME, error);
-										var answer = response.toJSON(),
-										    count = parseInt(answer.headers['compression-count']);
-
-										CURRENT_COUNT = count;
-										resolve();
-									} catch (err) {
-										throw new _gulpUtil.PluginError(PLUGIN_NAME, err);
-									}
-								});
-							} else {
-								resolve();
-							}
-						});
-
-					case 3:
-						if (!(CURRENT_COUNT >= MAX_COUNT)) {
-							_context3.next = 12;
-							break;
+					(0, _request2.default)({
+						url: 'https://api.tinypng.com/shrink',
+						method: 'POST',
+						strictSSL: !1,
+						headers: {
+							Accept: '*/*',
+							"Cache-Control": 'no-cache',
+							Authorization: 'Basic ' + AUTH_TOKEN
 						}
+					}, function (error, response, body) {
+						try {
+							if (error !== null) throw new _gulpUtil.PluginError(PLUGIN_NAME, error);
+							const answer = response.toJSON(),
+							      count = parseInt(answer.headers['compression-count']);
 
-						if (!(keys.length <= CURRENT_KEY + 1)) {
-							_context3.next = 6;
-							break;
+							CURRENT_COUNT = count;
+							resolve();
+						} catch (err) {
+							throw new _gulpUtil.PluginError(PLUGIN_NAME, err);
 						}
-
-						throw new _gulpUtil.PluginError(PLUGIN_NAME, "All api keys used!");
-
-					case 6:
-						CURRENT_KEY++;
-						CURRENT_COUNT = null;
-						_context3.next = 10;
-						return compileTest(keys, file, cb);
-
-					case 10:
-						_context3.next = 14;
-						break;
-
-					case 12:
-						apiKey = keys[CURRENT_KEY];
-
-						compile(file, size, apiKey, cb);
-
-					case 14:
-						_context3.next = 19;
-						break;
-
-					case 16:
-						_context3.prev = 16;
-						_context3.t0 = _context3["catch"](0);
-						throw new _gulpUtil.PluginError(PLUGIN_NAME, _context3.t0);
-
-					case 19:
-					case "end":
-						return _context3.stop();
+					});
+				} else {
+					resolve();
 				}
+			});
+			if (CURRENT_COUNT >= MAX_COUNT) {
+				if (keys.length <= CURRENT_KEY + 1) throw new _gulpUtil.PluginError(PLUGIN_NAME, "All api keys used!");
+				CURRENT_KEY++;
+				CURRENT_COUNT = null;
+				yield compileTest(keys, file, cb);
+			} else {
+				const apiKey = keys[CURRENT_KEY];
+				compile(file, size, apiKey, cb);
 			}
-		}, _callee3, void 0, [[0, 16]]);
-	}));
+		} catch (error) {
+			throw new _gulpUtil.PluginError(PLUGIN_NAME, error);
+		}
+	});
 
-	return function compileTest(_x6, _x7, _x8, _x9) {
-		return _ref4.apply(this, arguments);
+	return function compileTest(_x5, _x6, _x7, _x8) {
+		return _ref3.apply(this, arguments);
 	};
-}(),
-    tinypng = function tinypng(keys, file) {
-	var size = arguments.length > 2 && arguments[2] !== void 0 ? arguments[2] : [],
-	    exit_path = arguments[3],
-	    cb = arguments[4],
-	    tmpFileName = (0, _md2.default)(file.contents);
-
-	readTemp(tmpFileName, function (err, tmpFile) {
+})(),
+      tinypng = (keys, file, size = [], exit_path, cb) => {
+	const tmpFileName = (0, _md2.default)(file.contents);
+	readTemp(tmpFileName, (err, tmpFile) => {
 		if (err) {
 			compileTest(keys, file, size, cb);
 		} else {
 			try {
-				var _path = (0, _path2.parse)(file.path);
-				size.forEach(function (_ref5) {
-					var name = _ref5.name,
-					    dir = (0, _path2.join)(exit_path, name);
-
-					(0, _fs.access)(dir, _fs.F_OK, function (err) {
+				const _path = (0, _path2.parse)(file.path);
+				size.forEach(({ name }) => {
+					const dir = (0, _path2.join)(exit_path, name);
+					(0, _fs.access)(dir, _fs.F_OK, err => {
 						if (err) {
-							(0, _mkdirp2.default)(dir, function (err) {
+							(0, _mkdirp2.default)(dir, err => {
 								if (err) {
 									console.error('Error creating temp folder', err);
 								} else {
@@ -349,14 +248,9 @@ var PLUGIN_NAME = "gulp-tiny",
 		}
 	});
 };
-var gulpTiny = exports.gulpTiny = function gulpTiny() {
-	var options = arguments.length > 0 && arguments[0] !== void 0 ? arguments[0] : {},
-	    apiKeys = options.apiKeys,
-	    cached = options.cached,
-	    max_count = options.max_count,
-	    size = options.size,
-	    exit_path = options.exit_path;
 
+const gulpTiny = exports.gulpTiny = (options = {}) => {
+	const { apiKeys, cached, max_count, size, exit_path } = options;
 
 	MAX_COUNT = max_count || MAX_COUNT;
 
@@ -376,24 +270,21 @@ var gulpTiny = exports.gulpTiny = function gulpTiny() {
 		}
 
 		if (file.isBuffer()) {
-			var prevLength = file.contents.length,
-			    self = this;
+			const prevLength = file.contents.length,
+			      self = this;
 
 
-			tinypng(apiKeys, file, size, exit_path, function (data) {
-				var _path = (0, _path2.parse)(file.path);
+			tinypng(apiKeys, file, size, exit_path, data => {
+				const _path = (0, _path2.parse)(file.path);
 				file.contents = data[0].content;
 				self.push(file);
 
-				data.forEach(function (_ref6) {
-					var filename = _ref6.filename,
-					    folder = _ref6.folder;
-
+				data.forEach(({ filename, folder }) => {
 					if (folder) {
-						var dir = (0, _path2.join)(exit_path, folder);
-						(0, _fs.access)(dir, _fs.F_OK, function (err) {
+						const dir = (0, _path2.join)(exit_path, folder);
+						(0, _fs.access)(dir, _fs.F_OK, err => {
 							if (err) {
-								(0, _mkdirp2.default)(dir, function (err) {
+								(0, _mkdirp2.default)(dir, err => {
 									if (err) {
 										console.error('Error creating temp folder', err);
 									} else {
